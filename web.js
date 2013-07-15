f6ab5c023d45fdb471f02024f39eb67401744c6e
Marcelo Lanzarotti <hitus@hotmail.com>
2013-07-15 06:08:21 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var content;
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function(err, data){
-      if (err) throw err;
-  response.send(data.toString());
+  fs.readFileSync('./index.html', function read(err, data){
+      if (err) {
+	  throw err;
+      }
+  content = data;
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
